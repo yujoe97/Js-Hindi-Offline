ceba0e6e8f9bc207414dd01eb5b3ca2b8670a1b9
ABHIJIT NASKAR <140011306+yujoe97@users.noreply.github.com>
2023-09-30 19:42:54 +0000

    variables

---
+++ 01-basics/variables.js
@@ -0,0 +1,5 @@
+ const acountId = "Abhijit_81";
+ let accountEmail = "abhi.abhi@email.com";
+ accountPassword = 1246235;
+ accountCity = "Howrah";
+ console.table([acountId,accountEmail,accountPassword,accountCity]);
